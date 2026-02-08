ff3cf0df36eb7d5bf5bdc86722add55e2c1a0d94
Arobby71 <arobby1971@gmail.com>
2026-02-08 00:19:00 -0500

    Delete src directory

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import UNPartnershipDashboardEnhanced from "./UNPartnershipDashboardEnhanced.jsx";
-
-export default function App() {
-  return <UNPartnershipDashboardEnhanced />;
-}
--- src/components/ui/badge.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
-
-export function Badge({ className, ...props }) {
-  return (
-    <span className={cn("inline-flex items-center rounded-full border border-slate-200 bg-white px-2.5 py-0.5 text-xs font-semibold text-slate-900", className)} {...props} />
-  );
-}
