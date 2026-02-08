cfd2d9b4054f6ff2ce717f27f084045467b18ac9
Arobby71 <arobby1971@gmail.com>
2026-02-08 00:20:57 -0500

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-import animate from "tailwindcss-animate";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [animate],
-};
